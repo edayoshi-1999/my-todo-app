70ae7282415398ac5c9b8ee30256d43e6143482f
edayoshi-1999 <st24012@g.u-fukui.ac.jp>
2025-05-13 11:32:37 +0900

    feat(tasks): implement task management components

--- src/app/tasks/page.tsx
+++ src/app/tasks/page.tsx
@@ -1,3 +1,30 @@
+// ui/tasksのファイル
+import TaskList from "@/ui/tasks/task-list"; 
+import Search from "@/ui/tasks/search";
+import { SearchButton, EditButton, AddButton, DeleteButton } from "@/ui/tasks/button"; 
+import StatusFilter from "@/ui/tasks/status-filter";
+import SideBar from "@/ui/tasks/side-bar";
+
+// libのファイル
+
+
 export default function Page() {
-  return <p>タスク一覧 Page</p>;
+  return (
+    //一番上に、検索画面
+    //その下に、タスクの絞り込みフィルターと、追加ボタン
+    //その下に、タスクの一覧表示
+    //左に、サイドバー 
+    <div className="flex">
+      <SideBar />
+      <div className="flex flex-col w-full">
+        <Search />
+        <div className="flex justify-between items-center p-4">
+          <StatusFilter />
+          <AddButton />
+        </div>
+        <TaskList />
+      </div>
+    </div>
+
+  );
 }
